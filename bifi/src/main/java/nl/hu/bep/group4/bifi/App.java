45978b9570de0eb2675dc845667e35d83829b7d4
Matthijs Osterhaus <matthijs@mbpvanmatthijs.wifi.hu.nl>
2019-05-08 11:35:17 +0200

    Maven project added

---
+++ bifi/src/main/java/nl/hu/bep/group4/bifi/App.java
@@ -0,0 +1,13 @@
+package nl.hu.bep.group4.bifi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
