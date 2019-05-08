07d83e8d4563013cb6c60d794af76f302a98e8ce
JonathanTonckensHU <jonathan.tonckens@student.hu.nl>
2019-05-08 12:56:38 +0200

    Added access to legacy system and wrote some example code to see if it works

--- bifi/src/main/java/nl/hu/bep/group4/bifi/App.java
+++ bifi/src/main/java/nl/hu/bep/group4/bifi/App.java
@@ -1,13 +1,20 @@
 package nl.hu.bep.group4.bifi;
 
+import ADDRLOOKUPER.AddressLookerUPAlreadyCloosed;
+import ADDRLOOKUPER.AddressLookerUPAlreadyLookinUP;
+import ADDRLOOKUPER.AddressLookerUPAlreadyReadyToLookUP;
+import ADDRLOOKUPER.AddressLookerUpInWrongStateException;
+import ADDRLOOKUPER.LOOKUP_AdDDR;
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws AddressLookerUpInWrongStateException, AddressLookerUPAlreadyLookinUP, AddressLookerUPAlreadyReadyToLookUP, AddressLookerUPAlreadyCloosed
     {
-        System.out.println( "Hello World!" );
+    	LOOKUP_AdDDR.scanStart();
+    	System.out.println(LOOKUP_AdDDR.scanForward("MOATA"));
+    	LOOKUP_AdDDR.scanStop();
     }
 }
