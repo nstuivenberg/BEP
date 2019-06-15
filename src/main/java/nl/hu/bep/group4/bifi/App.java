638420e572e135fe3568793289565459c4dac436
nick.stuivenberg <nick.stuivenberg@cgi.com>
2019-06-15 13:39:01 +0200

    Lege tests weggepleurd

--- src/main/java/nl/hu/bep/group4/bifi/App.java
+++ src/main/java/nl/hu/bep/group4/bifi/App.java
@@ -12,7 +12,6 @@ public class App
     public static void main(String[] args) throws ClassNotFoundException, GarbageDataException, SQLException, IOException
     {
     	Controller controller = new Controller(FactuurLaderFactory.createFactuurLader(), IEFExporterFactory.createIEFExporter());
-
         controller.main(args);
     }
 }
